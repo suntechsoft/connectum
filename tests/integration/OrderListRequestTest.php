bc99eeb91a9e283c950a4dd7e25031150ec0f9b2
lashnev <a.lashnev@platron.ru>
2017-11-08 15:04:12 +0300

    Сортировка по типу

--- tests/integration/OrderListRequestTest.php
+++ tests/integration/OrderListRequestTest.php
@@ -3,13 +3,19 @@
 namespace Platron\Connectum\tests\integration;
 
 use Platron\Connectum\clients\GetClient;
+use Platron\Connectum\data_objects\CardData;
 use Platron\Connectum\services\order_list\OrderListRequest;
 use Platron\Connectum\services\order_list\OrderListResponse;
+use Platron\Connectum\handbooks\OrderStatus;
 
 class OrderListRequestTest extends IntegrationTestBase {
     public function testRequest(){
         $client = (new GetClient($this->login, $this->password, $this->certificatePath, $this->certificatePassword))->setTest()->setLogger($this->logger);
         $request = new OrderListRequest();
+        
+        $card = new CardData();
+        $card->type = OrderStatus::PREPARED;
+        $request->setCard($card);
         
         $this->assertTrue((new OrderListResponse($client->sendRequest($request)))->isValid());
     }
