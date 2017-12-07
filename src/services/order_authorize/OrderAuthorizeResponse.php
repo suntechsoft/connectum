f7d25749d0109433758448c65bf9ceb23ba15391
lashnev <a.lashnev@platron.ru>
2017-12-07 16:17:41 +0300

    Fix auth response

--- src/services/order_authorize/OrderAuthorizeResponse.php
+++ src/services/order_authorize/OrderAuthorizeResponse.php
@@ -9,10 +9,7 @@ use Platron\Connectum\data_objects\Form3dData;
 class OrderAuthorizeResponse extends BaseResponse {
     /** @var OrderInfoResponse */
     public $order;
-    /** @var Form3dData */
-    public $form3d;
-    public $form3d_html;
-    
+
     public function __construct(\stdClass $response) {
         parent::__construct($response);
         if(!empty($response->orders[0])){
