65599ad7a932018ade8b742649a401631004e970
schursin <schursin@gmail.com>
2017-12-25 13:06:05 +0300

    Update Expands.php

--- src/handbooks/Expands.php
+++ src/handbooks/Expands.php
@@ -7,9 +7,10 @@ class Expands {
         CARD = 'card',
         CLIENT = 'client',
         LOCATION = 'location',
-        CUSTOM_FIELDS = 'issuer',
+        CUSTOM_FIELDS = 'custom_fields',
+        ISSUER = 'issuer',
         SECURE3D = 'sucure3d',
-        OPERATIONS_CASHFLOW = 'cashflow';
+        OPERATIONS_CASHFLOW = 'operations.cashflow';
     
     public static function getAllExpands(){
         return array(
@@ -17,6 +18,7 @@ class Expands {
             self::CLIENT,
             self::LOCATION,
             self::CUSTOM_FIELDS,
+            self::ISSUER,
             self::SECURE3D,
             self::OPERATIONS_CASHFLOW,
         );
