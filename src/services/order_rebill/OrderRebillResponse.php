4c731508907fbffcf20f5ccd8774ac389928bf43
lashnev <a.lashnev@platron.ru>
2018-07-23 17:35:21 +0300

    Order in response rebill

--- src/services/order_rebill/OrderRebillResponse.php
+++ src/services/order_rebill/OrderRebillResponse.php
@@ -6,6 +6,13 @@ use Platron\Connectum\services\BaseResponse;
 use Platron\Connectum\services\order_info\OrderInfoResponse;
 
 class OrderRebillResponse extends BaseResponse {
-    /** @var OrderInfoResponse */
-    public $orders;
+	/** @var OrderInfoResponse */
+	public $order;
+
+	public function __construct(\stdClass $response) {
+		parent::__construct($response);
+		if(!empty($response->orders[0])){
+			$this->order = new OrderInfoResponse($response);
+		}
+	}
 }
