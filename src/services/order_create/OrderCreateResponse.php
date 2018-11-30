cdb4785c035f75f6a87db677464a138222302826
pshiposha <p.shyposha@globalgames.net>
2018-11-30 15:39:26 +0200

    add getRedirectUrl

--- src/services/order_create/OrderCreateResponse.php
+++ src/services/order_create/OrderCreateResponse.php
@@ -5,14 +5,36 @@ namespace Platron\Connectum\services\order_create;
 use Platron\Connectum\services\BaseResponse;
 use Platron\Connectum\services\order_info\OrderInfoResponse;
 
-class OrderCreateResponse extends BaseResponse {
+class OrderCreateResponse extends BaseResponse
+{
+
     /** @var OrderInfoResponse */
     public $order;
-    
-    public function __construct(\stdClass $response) {
+
+    public function __construct(\stdClass $response)
+    {
         parent::__construct($response);
-        if(!empty($response->orders[0])){
+        if (!empty($response->orders[0])) {
             $this->order = new OrderInfoResponse($response);
         }
+    }
+
+
+    /**
+     * @param $isSandbox bool
+     * @return null|string
+     */
+    public function getRedirectUrl($isSandbox)
+    {
+        if (isset($this->order->id)) {
+            if (isset($isSandbox) && $isSandbox) {
+
+                return sprintf(self::REDIRECT_URL_SANDBOX, $this->order->id);
+            }
+
+            return sprintf(self::REDIRECT_URL, $this->order->id);
+        }
+
+        return null;
     }
 }
